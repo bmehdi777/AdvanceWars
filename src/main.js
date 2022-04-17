d537fea01dab12dc793e5ca1e66a610d79d865ae
bmehdi <mehdi.bentouati76@gmail.com>
2022-04-17 19:39:52 +0200

    [INIT] setup game

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ const app = new PIXI.Application({
     width: 720,
     height: 1280,
     backgroundColor: 0x1099bb,
-    view: document.querySelector("#game"),
 });
 
+document.querySelector("#game").appendChild(app.view);
 // Update function
 app.ticker.add((delta) => {});
